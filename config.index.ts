d54f48249d57a8d155830df13f8425d5ab3eeb23
Chun Sing Pang <atelierkarin@gmail.com>
2021-07-04 22:05:02 +0900

    update config index to use the real one

--- config.index.ts
+++ config.index.ts
@@ -4,9 +4,18 @@ const targetPath = './src/environments/environment.prod.ts';
 
 const envConfigFile = `export const environment = {
     production: true,
-    firebase: {},
+    firebase: {
+        apiKey: "${process.env.FIREBASE_API_KEY}",
+        authDomain: "fm-j-league-pack.firebaseapp.com",
+        databaseURL: "https://fm-j-league-pack.firebaseio.com",
+        projectId: "fm-j-league-pack",
+        storageBucket: "fm-j-league-pack.appspot.com",
+        messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+        appId: "${process.env.FIREBASE_APP_ID}",
+        measurementId: "${process.env.FIREBASE_MEASUREMENT_ID}"
+    },
     analytics: {
-        id: '${process.env.GOOGLE_ANAYLTICS_ID}'
+        id: "${process.env.GOOGLE_ANAYLTICS_ID}"
     }
 };
 `;
